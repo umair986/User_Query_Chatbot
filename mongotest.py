20e91e7d3e207f7bbb6e0bec7b4a1802ae858fba
Mohammed Umair Shaikh <69787249+umair986@users.noreply.github.com>
2025-04-11 20:45:11 +0530

    Update mongotest.py

--- mongotest.py
+++ mongotest.py
@@ -1,3 +1,3 @@
 from pymongo import MongoClient
-client = MongoClient("mongodb+srv://mohumair1901:mohumair1901@cluster0.r4h0l.mongodb.net/")
+client = MongoClient("")
 print(client.list_database_names())
